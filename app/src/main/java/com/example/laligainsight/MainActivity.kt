ea40a7f609146a31f81fa7f0cc6840ae9e21dbfd
cesaralonso4 <cesaratleti05@gmail.com>
2026-04-13 10:33:01 +0200

    implementación completa de detalle de equipo + base de detalle de jugador

--- app/src/main/java/com/example/laligainsight/MainActivity.kt
+++ app/src/main/java/com/example/laligainsight/MainActivity.kt
@@ -3,51 +3,92 @@ package com.example.laligainsight
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.lifecycleScope
 import com.example.laligainsight.api.RetrofitCliente
+import com.example.laligainsight.iu.TeamDetailScreen
 import com.example.laligainsight.iu.TeamsScreen
+import com.example.laligainsight.modelo.Team
 import kotlinx.coroutines.launch
+import androidx.compose.runtime.setValue
+import com.example.laligainsight.iu.PlayerDetailScreen
+import com.example.laligainsight.modelo.Player
 
 class MainActivity : ComponentActivity() {
+
+    // CAMBIO feature/detalle-equipo:
+    // Variable donde guardamos el equipo pulsado
+    // si es null ---> pantalla principal
+    // si no --> mostramos el detalle del equipo
+    private var selectedTeam by mutableStateOf<Team?>(null)
+
+    // CAMBIO feature/detalle-equipo:
+    // Variable donde guardamos la lista de equipos obtenidos por la  api
+    // De esta manera, no llamamos a setContent tantas veces
+    private var teams by mutableStateOf<List<Team>>(emptyList())
+
+    // Variable para el juagdor seleccionado
+    private var selectedPlayer by mutableStateOf<Player?>(null)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Al iniciar la app mostramos la pantalla Compose vacía
+        // Cambio feature/detalle-equipo
+        // Llamamos a setContent una vez
         setContent {
-            TeamsScreen(teams = emptyList())
-        }
-
-        // Llamamos a la API usando corrutinas
-        lifecycleScope.launch {
-            try {
-                // Petición a la API para obtener los equipos
-                val response = RetrofitCliente.api.getTeams()
-
-                // Si la respuesta es correcta
-                if (response.isSuccessful) {
-
-                    // Obtenemos la lista de equipos
-                    val teams = response.body()?.teams ?: emptyList()
-
-                    // Volvemos a pintar la pantalla, ahora con los datos reales
-                    setContent {
-                        TeamsScreen(teams = teams)
-                    }
-
-                } else {
-                    // Si hay error, mostramos la pantalla sin datos
-                    setContent {
-                        TeamsScreen(teams = emptyList())
-                    }
+            when {
+                selectedPlayer != null -> {
+                    PlayerDetailScreen(
+                        player = selectedPlayer!!,
+                        onBackClick = { selectedPlayer = null }
+                    )
                 }
 
-            } catch (e: Exception) {
-                // Si ocurre un error de conexión, mostramos la pantalla vacía
-                setContent {
-                    TeamsScreen(teams = emptyList())
+                selectedTeam != null -> {
+                    TeamDetailScreen(
+                        team = selectedTeam!!,
+                        onBackClick = { selectedTeam = null },
+                        onPlayerClick = { player ->
+                            selectedPlayer = player
+                        }
+                    )
+                }
+
+                else -> {
+                    TeamsScreen(
+                        teams = teams,
+                        onTeamClick = { team ->
+                            selectedTeam = team
+                        }
+                    )
+                }
+            }
+        }
+
+            // Llamamos a la API usando corrutinas
+            lifecycleScope.launch {
+                try {
+                    // Petición a la API para obtener los equipos
+                    val response = RetrofitCliente.api.getTeams()
+
+                    // Si la respuesta es correcta
+                    if (response.isSuccessful) {
+
+                        // Obtenemos la lista de equipos
+                        teams = response.body()?.teams ?: emptyList()
+
+                    } else {
+                        // Mostramos la lista vacía si la respuesta no es correcta
+                        teams = emptyList()
+                    }
+
+                } catch (e: Exception) {
+                    // Si ocurre un error de conexión, mostramos la lista vacía
+                    teams = emptyList()
                 }
             }
         }
     }
-}
+
--- app/src/main/java/com/example/laligainsight/modelo/Team.kt
+++ app/src/main/java/com/example/laligainsight/modelo/Team.kt
@@ -1,10 +1,12 @@
 package com.example.laligainsight.modelo
 
+import com.google.gson.annotations.SerializedName
+
 // Completamos la data class Team, añadiendo las variables id y crest (escudo)
 data class Team(
     val id: Int,
-    val name: String,
-    val crest: String,
+    @SerializedName("name") val name: String,
+    @SerializedName("crest") val crest: String,
     val venue: String? = null
 )
 
